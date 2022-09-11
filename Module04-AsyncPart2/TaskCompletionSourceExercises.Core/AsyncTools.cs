4f0bb53c0055c5dba8806ec229866ef677bba596
Vitaly Senokosov <6545415@gmail.com>
2022-09-11 13:54:38 +0300

    Module04-AsyncPart2 AsyncTools

--- Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
+++ Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
@@ -4,11 +4,39 @@ using System.Threading.Tasks;
 
 namespace TaskCompletionSourceExercises.Core
 {
-    public class AsyncTools
+    public static class AsyncTools
     {
         public static Task<string> RunProgramAsync(string path, string args = "")
         {
-            return Task.FromResult(string.Empty);
+            var process = new Process();
+            process.EnableRaisingEvents = true;
+            process.StartInfo = new ProcessStartInfo(path, args)
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            var tcs = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
+            process.Exited += async (sender, eventArgs) =>
+            {
+                var senderProcess = sender as Process;
+                if (senderProcess is null)
+                    return;
+                if (senderProcess.ExitCode != 0)
+                {
+                    var output = await process.StandardError.ReadToEndAsync();
+                    tcs.SetException(new Exception(output));
+                }
+                else
+                {
+                    var output = await process.StandardOutput.ReadToEndAsync();
+                    tcs.SetResult(output);
+                }
+            };
+
+            process.Start();
+
+            return tcs.Task;
         }
     }
 }
